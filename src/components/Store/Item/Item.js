49535654262b96dd1a39dc91fae9981fdf24d96d
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-11 01:41:29 +0100

    class name added

--- src/components/Store/Item/Item.js
+++ src/components/Store/Item/Item.js
@@ -26,18 +26,20 @@ const Item = ({item, addToCart, index}) => {
       <div className="imgWrapper">
         <img src={item.img_url} width={200} height={200}></img>
       </div> 
-      <span>{item.name}</span>
-      <input
-        type="number"
-        value={value}
-        min={0}
-        max={item.quantity}
-        onChange={onChangeHandler}
-      />
-      <button onClick={submitHandler} className={disable}  disabled={disable}>
-        Add to Cart
-      </button>
-      <span className='inStock label'>{item.quantity === 0? "Out of Stock": `In stock: ${item.quantity}`}</span>
+      <div className={"description"}>
+        <span>{item.name}</span>
+        <input
+          type="number"
+          value={value}
+          min={0}
+          max={item.quantity}
+          onChange={onChangeHandler}
+        />
+        <button onClick={submitHandler} className={disable}  disabled={disable}>
+          Add to Cart
+        </button>
+        <span className='inStock label'>{item.quantity === 0? "Out of Stock": `In stock: ${item.quantity}`}</span>
+      </div>
     </li>
   );
 };
